78379c98ea5059d7045ecce1b6a2144db9c31ddb
mbojer <morten.bojer@gmail.com>
2018-04-15 16:34:46 +0100

    230v changed

--- 230vON.sh
+++ 230vON.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-Settings_Topic[1]='/Boat/Relay/WR1'
-Settings_Payload[1]='4-ON'
+Settings_Topic[1]='/Boat/Relay/WR1/4'
+Settings_Payload[1]='1'
 
-Settings_Topic[2]='/Boat/Relay/WR1'
-Settings_Payload[2]='5-ON'
+Settings_Topic[2]='/Boat/Relay/WR1/5'
+Settings_Payload[2]='1'
 
 for (( i=1; i<=${#Settings_Topic[@]}; i++ ))
 do
   mosquitto_pub -h localhost -t ${Settings_Topic[i]} -m ${Settings_Payload[i]} -u DasBoot -P NoSinking
-  sleep 0.750
+  sleep 0.250
 done
 
 exit 0
